8908dca940c5c285c8a17ce1cb90d345ab4b147e
Mark Smith <smith.h.mark@gmail.com>
2018-04-13 10:08:12 -0700

    reifying config, option to use completions

--- brute.go
+++ brute.go
@@ -54,14 +54,7 @@ func addSquare(h *OrderedSquares, sqr *SimpleSquare, n int) {
 func BruteN(f *Field, n int) []*SimpleSquare {
 	maxSqs := make(OrderedSquares, 0, n)
 	heap.Init(&maxSqs)
-	xCandidates := make([]int, f.XDim)
-	yCandidates := make([]int, f.YDim)
-	var mergedCandidates []int
-	if f.XDim > f.YDim {
-		mergedCandidates = make([]int, 0, f.XDim)
-	} else {
-		mergedCandidates = make([]int, 0, f.YDim)
-	}
+	cfg := config(f, false)
 	for Py := 0 ; Py < f.YDim ; Py++ {
 		if len(maxSqs) > 0 && Py > f.YDim - maxSqs[0].Side {
 			continue
@@ -71,7 +64,7 @@ func BruteN(f *Field, n int) []*SimpleSquare {
 				continue
 			}
 			if f.Get(Px, Py) == 1 {
-				largestS := largestSquareAt(f, Px,Py, xCandidates, yCandidates, mergedCandidates)
+				largestS := largestSquareAt(f, Px,Py, cfg)
 				addSquare(&maxSqs, &largestS, n)
 			}
 		}
@@ -88,8 +81,43 @@ func BruteN(f *Field, n int) []*SimpleSquare {
 	return ret
 }
 
-func Brute(f *Field) *SimpleSquare {
-	var maxS *SimpleSquare
+func completions(f *Field) ([][]int, [][]int) {
+	horts := make([][]int, f.XDim)
+	verts := make([][]int, f.XDim)
+	for col := 0 ; col < f.XDim ; col++ {
+		verts[col] = make([]int,f.YDim)
+		horts[col] = make([]int,f.YDim)
+	}
+	for row := f.YDim -1 ; row >= 0 ; row-- {
+		for col := f.XDim -1 ; col >= 0 ; col-- {
+			val := f.Get(row, col)
+			if val != 0 {
+				if col == f.XDim - 1 {
+					horts[col][row] = 1
+				} else {
+					tmp := horts[col+1][row]
+					horts[col][row] = tmp + 1
+				}
+				if row == f.YDim -1 {
+					verts[col][row] = 1
+				} else {
+					tmp := verts[col][row+1]
+					verts[col][row] = tmp + 1
+				}
+			}
+		}
+	}
+	return horts, verts
+}
+
+type largestSquareData struct {
+	xCandidates []int
+	yCandidates []int
+	mergedCandidates []int
+	horts, verts [][]int
+}
+
+func config(f *Field, useLookups bool) *largestSquareData {
 	xCandidates := make([]int, f.XDim)
 	yCandidates := make([]int, f.YDim)
 	var mergedCandidates []int
@@ -98,6 +126,20 @@ func Brute(f *Field) *SimpleSquare {
 	} else {
 		mergedCandidates = make([]int, 0, f.YDim)
 	}
+	var horts, verts [][]int
+	if useLookups {
+		horts, verts = completions(f)
+	} else {
+		horts, verts = make([][]int,0), make([][]int,0)
+	}
+	conf := largestSquareData{xCandidates,yCandidates,mergedCandidates, horts,verts}
+	return &conf
+}
+
+func Brute(f *Field) *SimpleSquare {
+	var maxS *SimpleSquare
+	//log.Printf("Completed Setup")
+	cfg := config(f, false)
 	for Py := 0 ; Py < f.YDim ; Py++ {
 		if maxS != nil && Py > f.YDim - maxS.Side {
 			continue
@@ -107,11 +149,12 @@ func Brute(f *Field) *SimpleSquare {
 				continue
 			}
 			if f.Get(Px, Py) == 1 {
-				largestS := largestSquareAt(f, Px,Py, xCandidates, yCandidates, mergedCandidates)
+				largestS := largestSquareAt(f, Px,Py, cfg)
 				if largestS.Side > 0 {
 					if maxS == nil {
 						maxS = &largestS
 					} else if largestS.Side > maxS.Side {
+						//log.Printf("Found new largest square:%v", largestS)
 						maxS = &largestS
 					} 
 				}
@@ -121,8 +164,13 @@ func Brute(f *Field) *SimpleSquare {
 	return maxS
 }
 
-func largestSquareAt(f *Field, px, py int, xCandidates, yCandidates, mergedCandidates []int) SimpleSquare {
+func largestSquareAt(f *Field, px, py int, cfg *largestSquareData) SimpleSquare {
 	ii := 1
+	xCandidates := cfg.xCandidates
+	yCandidates := cfg.yCandidates
+	mergedCandidates := cfg.mergedCandidates
+	horts := cfg.horts
+	verts := cfg.verts
 	curXCand, curYCand := -1, -1
 	//log.Printf("Walking horizontally looking for possible side lengths")
 	for px+ii <f.XDim && f.Get(px+ii, py) == 1 && py+1 < f.YDim{
@@ -147,7 +195,7 @@ func largestSquareAt(f *Field, px, py int, xCandidates, yCandidates, mergedCandi
 	//log.Printf("candidate sizes:%v", candidates)
 
 	for _, side := range candidates {
-		if completesSquare(f, px,py, side){
+		if completesSquare(f, px,py, side, horts, verts){
 			//log.Printf("found a square:%v:%v:%v", px,py,side)
 			return SimpleSquare{px,py,side+1}
 		}
@@ -156,17 +204,25 @@ func largestSquareAt(f *Field, px, py int, xCandidates, yCandidates, mergedCandi
 	return SimpleSquare{0,0,0}
 }
 
-func completesSquare(f *Field, px,py, side int) bool {
-	for ii := 0 ; ii < side ; ii++ {
-		rowVal := f.Get(px+ii, py+side)
-		colVal := f.Get(px+side, py+ii)
-		sum := rowVal + colVal
-		//sum := f.Get(px+ii, py+side-1) + f.Get(px+side-1, py+ii)
-		if sum != 2 {
+func completesSquare(f *Field, px,py, side int, horts, verts [][]int) bool {
+	if len(horts) > 0 {
+		if horts[px][py +side] >= side && verts[px+side][py] >= side {
+			return true
+		} else {
 			return false
 		}
+	} else {
+		for ii := 0 ; ii < side ; ii++ {
+			rowVal := f.Get(px+ii, py+side)
+			colVal := f.Get(px+side, py+ii)
+			sum := rowVal + colVal
+			//sum := f.Get(px+ii, py+side-1) + f.Get(px+side-1, py+ii)
+			if sum != 2 {
+				return false
+			}
+		}
+		return true
 	}
-	return true
 }
 
 func pop(s *sl.Stack) int {
